f74be7e4a48428d7493c016e791b1d2e2a2f06d4
Loc Truong <loc.truong@codehq.nz>
2024-03-14 14:14:12 +0700

    setup api run once

--- projects/LambdaTest/pages/cart.page.ts
+++ projects/LambdaTest/pages/cart.page.ts
@@ -2,22 +2,21 @@ import { Page, expect } from "@playwright/test";
 import { BasePage } from "./base.page";
 import ProductInfo from "../fixtures/model/productInfo";
 
-export class Cart extends BasePage{
-    constructor(page:Page){
-        super(page)
+export class Cart extends BasePage {
+    constructor(page: Page) {
+        super(page);
     }
     getSelectedProduct(name: any) {
-        let normalizeName = name.replace('"', '')
-        return this.page.locator("div#checkout-cart tr:has-text('" + normalizeName + "')")
+        return this.page.locator(`div#checkout-cart tr:has-text('${name}')`);
     }
 
     async validateShoppingCart(productInfo: ProductInfo) {
-        console.log(`Product Info ${JSON.stringify(productInfo)}`)
-        const productRow = this.getSelectedProduct(productInfo.name)
-        const unitPrice = await productRow.locator('td').nth(4).textContent()
-        expect(unitPrice).toContain(productInfo.price.toString())
-        let quantityPrice = await productRow.locator('td').nth(5).textContent()
-        let calPrice = Number(productInfo.price * productInfo.quantity)
-        expect(quantityPrice).toContain(calPrice.toString())
+        console.log(`Product Info ${JSON.stringify(productInfo)}`);
+        const productRow = this.getSelectedProduct(productInfo.name);
+        const unitPrice = await productRow.locator("td").nth(4).textContent();
+        expect(unitPrice).toContain(productInfo.price.toString());
+        let quantityPrice = await productRow.locator("td").nth(5).textContent();
+        let calPrice = Number(productInfo.price * productInfo.quantity);
+        expect(quantityPrice).toContain(calPrice.toString());
     }
 }
