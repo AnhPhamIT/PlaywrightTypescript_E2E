f8d0a67978a9d0e93c4a085a436ec3d85c710d78
harmoney-anh <anh.pham@codehq.nz>
2024-04-15 18:00:43 +0700

    chromatic

--- projects/LambdaTest/tests/e2e/VisualTesting.spec.ts
+++ projects/LambdaTest/tests/e2e/VisualTesting.spec.ts
@@ -10,8 +10,8 @@ test.beforeEach(async ({ app }) => {
 test("@only Should able to search then checkout a product", async ({ app }, testInfo) => {
     let productInfo: Product;
     await app.topbarPage.searchProductByName("Nikon D300");
-    // await app.searchPage.validateProductImageByIndex(1);
-    await takeSnapshot(app.page, testInfo);
+    await app.searchPage.validateProductImageByIndex(1);
+    // await takeSnapshot(app.page, testInfo);
     await app.filterComponent.filterBy("In stock");
     productInfo = await app.itemComponent.addProductToCart(1);
     await app.basePage.selectActionOnNotification("Checkout");
