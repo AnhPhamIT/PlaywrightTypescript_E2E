4abab11cbff29fb600200a3251cf35a0a61279ee
harmoney-anh <anh.pham@codehq.nz>
2024-04-15 18:39:48 +0700

    disable filter

--- projects/LambdaTest/tests/e2e/VisualTesting.spec.ts
+++ projects/LambdaTest/tests/e2e/VisualTesting.spec.ts
@@ -12,7 +12,7 @@ test("@only Should able to search then checkout a product", async ({ app }, test
     await app.topbarPage.searchProductByName("Nikon D300");
     await app.searchPage.validateProductImageByIndex(1);
     // await takeSnapshot(app.page, testInfo);
-    await app.filterComponent.filterBy("In stock");
+    // await app.filterComponent.filterBy("In stock");
     productInfo = await app.itemComponent.addProductToCart(1);
     await app.basePage.selectActionOnNotification("Checkout");
     await app.checkoutPage.validateCheckoutItem(productInfo);
